1d5843c9d532a2e4f597ad826dfbaddd2f46e3d1
stelsp <stelspeh@gmail.com>
2022-08-22 10:09:35 +0300

    feat: initial setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # whymade-beats-store
+
 Website for the whymade beatmaker
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './scss/global.scss';
+
+const App: FC = () => {
+  return <div>hello</div>;
+};
+
+export default App;
