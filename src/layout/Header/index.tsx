0153b5636490e055284129db70f510081097cf9e
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-21 15:58:17 +0300

    feat: add Header

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import style from './Header.module.scss';
+
+const Header: FC = () => {
+  return (
+    <header className={style.header} id={'header'}>
+      <div className={style.container}>
+        <Link to={'/'} className={style.logo}>
+          WHYMADE
+        </Link>
+        <nav className={style.nav}>
+          <Link to={'/#beats'}>Beats</Link>
+          <Link to={'/#prod'}>Prod.</Link>
+          <Link to={'/#contacts'}>Contacts</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
