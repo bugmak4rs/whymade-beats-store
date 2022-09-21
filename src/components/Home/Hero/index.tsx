93ef9e406a0c2edb35e3732535218898e51cb40a
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-21 15:57:46 +0300

    feat: add Hero for Home

---
+++ src/components/Home/Hero/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import style from './Hero.module.scss';
+
+const Hero: FC = () => {
+  return (
+    <section className={style.hero} id={'hero'}>
+      <div className={style.container}>
+        <h1 className={style.title}>Title</h1>
+        <p className={style.text}>I make the freshest beats in all the latest styles.</p>
+        <Link to={'/beats'} className={style.link}>
+          get swag
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
