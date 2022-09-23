b691b4e42f1ab885c95715341be81fcd48e2bdc8
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-23 08:10:49 +0300

    feat: refactoring Beats

--- src/components/Home/Beats/index.tsx
+++ src/components/Home/Beats/index.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react';
 import BeatsCard from '../../Cards/Home/BeatsCard';
 
+import iayze from '../../../assets/img/beats/iayze.png';
+import slumps from '../../../assets/img/beats/6slumps.png';
+import kencarson from '../../../assets/img/beats/ken-carson.png';
+import richamiri from '../../../assets/img/beats/rich-amiri.png';
+
 import style from './Beats.module.scss';
 
 const Beats: FC = () => {
@@ -10,11 +15,10 @@ const Beats: FC = () => {
         <div className={style.offset}>
           <h2 className={style.title}>Beats</h2>
           <div className={style.cards}>
-            <BeatsCard image={''} />
-            <BeatsCard image={''} />
-            <BeatsCard image={''} />
-            <BeatsCard image={''} />
-            <BeatsCard image={''} />
+            <BeatsCard image={iayze} price={20} bpm={133} name={'UP!'} />
+            <BeatsCard image={slumps} price={50} bpm={142} name={'hood&friends'} />
+            <BeatsCard image={kencarson} price={30} bpm={148} name={'planets'} />
+            <BeatsCard image={richamiri} price={20} bpm={147} name={'Love off'} />
           </div>
         </div>
       </div>
