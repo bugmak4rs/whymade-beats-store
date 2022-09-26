5897dcd61de534f70fc2ff430a232286f24554a3
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-26 07:09:29 +0300

    feat: refactoring card Prod

--- src/components/Cards/Home/ProdCard/index.tsx
+++ src/components/Cards/Home/ProdCard/index.tsx
@@ -1,24 +1,28 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import style from './Prod.module.scss';
 
 interface IProdCard {
   image: string;
-  price: number;
-  bpm: number;
   name: string;
+  track: string;
 }
 
-const ProdCard: FC<IProdCard> = ({ image, price, bpm, name }) => {
+const ProdCard: FC<IProdCard> = ({ image, name, track }) => {
   return (
     <div className={style.card}>
-      <img src={image} className={style.image} />
-      <div className={style.content}>
-        <p>
-          <span>{price}$</span> • {bpm} BPM{' '}
-        </p>
-        <span>{name}</span>
-      </div>
+      <Link to="">
+        <img src={image} className={style.image} />
+      </Link>
+      <Link to="">
+        <div className={style.content}>
+          <p>
+            <span>{name}</span>
+          </p>
+          <span>{track}</span>
+        </div>
+      </Link>
     </div>
   );
 };
