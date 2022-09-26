93d3bde70cddb6781b693fff778cd2b7411c9d69
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-26 10:51:42 +0300

    feat: add page Main

---
+++ src/pages/AboutMe/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import Hero from '../../components/AboutMe/Hero';
+
+const AboutMe: FC = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default AboutMe;
