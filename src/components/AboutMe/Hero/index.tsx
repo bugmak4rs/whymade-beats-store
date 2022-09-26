a05f0c5829a343ba8c56f33e73efeab4ec8446d9
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-26 12:32:42 +0300

    feat: add socials + new classes

--- src/components/AboutMe/Hero/index.tsx
+++ src/components/AboutMe/Hero/index.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
 
 import Me from '../../../assets/img/about-me/whymade.jpg';
+import telegram from '../../../assets/icons/telegram.svg';
+import instagram from '../../../assets/icons/instagram.svg';
+import vk from '../../../assets/icons/vk.svg';
 
 import style from './AboutMe.module.scss';
 
@@ -10,7 +13,7 @@ const Hero: FC = () => {
       <div className={style.container}>
         <div className={style.offset}>
           <div className={style.content}>
-            <div>
+            <div className={style.top}>
               <h1 className={style.title}>Yo, I'm WhyMade</h1>
               <p className={style.vocation}>Producer + Loopmaker</p>
               <p className={style.credits}>
@@ -18,7 +21,7 @@ const Hero: FC = () => {
                 163joint, twin.kami, Sunexite, wth_dp, Globrooks, Pluton, Lil Naly & more
               </p>
             </div>
-            <div>
+            <div className={style.bottom}>
               <p className={style.offers}>
                 All offers -{'>'}{' '}
                 <a href="" target={'_blank'}>
@@ -26,6 +29,17 @@ const Hero: FC = () => {
                 </a>{' '}
                 inst
               </p>
+              <div className={style.socials}>
+                <a href={'https://t.me/whymade'} target={'_blank'}>
+                  <img src={telegram} alt="Telegram" />
+                </a>
+                <a href={''} target={'_blank'}>
+                  <img src={instagram} alt="Instagram" />
+                </a>
+                <a href={'https://vk.com/whymade'} target={'_blank'}>
+                  <img src={vk} alt="Vk" />
+                </a>
+              </div>
             </div>
           </div>
           <img src={Me} alt="Me" className={style.image} />
