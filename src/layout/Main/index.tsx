28d3c21214aebf5551e24773431bf37431da3bb9
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-26 15:17:46 +0300

    feat: add team and refactoring index.tsx

--- src/layout/Main/index.tsx
+++ src/layout/Main/index.tsx
@@ -1,18 +1,21 @@
 import React, { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Beats from '../../pages/Beats';
-import AboutMe from '../../pages/AboutMe';
 import Home from '../../pages/Home';
+import AboutMe from '../../pages/AboutMe';
+import Beats from '../../pages/Beats';
+import Team from '../../pages/Team';
+import NotFound from '../../pages/NotFound';
 
 const Main: FC = () => {
   return (
     <main>
       <Routes>
         <Route path={'/'} element={<Home />} />
-        <Route path={'/beats'} element={<Beats />} />
         <Route path={'/about-me'} element={<AboutMe />} />
-        {/* <Route path={'*'} element={<NotFound />} /> */}
+        <Route path={'/beats'} element={<Beats />} />
+        <Route path={'/team'} element={<Team />} />
+        <Route path={'*'} element={<NotFound />} />
       </Routes>
     </main>
   );
