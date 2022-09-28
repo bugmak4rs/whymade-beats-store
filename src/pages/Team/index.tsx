97920507fcefd0a545aaccd29e52dc56c9f4aaa6
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-28 16:50:37 +0700

    feat: update team

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,7 +1,82 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import TeamCard from '../../components/Cards/Team/TeamCards';
+
+import aleksey from '../../assets/img/team/aleksey.jpg';
+import slava from '../../assets/img/team/slava.jpg';
+
+import style from './Team.module.scss';
 
 const Team: FC = () => {
-  return <></>;
+  return (
+    <section className={style.team} id={'team'}>
+      <div className={style.container}>
+        <Link to={'/'} className={style.back}>
+          Назад
+        </Link>
+        <h1 className={style.title}>Команда разработчиков</h1>
+        <div className={style.collums}>
+          <div className={style.frontend}>
+            <div className={style.direction}>
+              <span className={style.direction__name}>{'<Frontend />'}</span>
+            </div>
+            <div className={style.cards}>
+              <TeamCard
+                image={aleksey}
+                name={'Aleksey Popov'}
+                telegram={'https://t.me/AlekseyPopovDev'}
+                github={'https://github.com/1PopovAleksey'}
+                description={
+                  'MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS'
+                }
+              />
+              <TeamCard
+                image={slava}
+                name={'Slava Kagarlitskiy'}
+                telegram={'https://t.me/StelsP'}
+                github={'https://github.com/StelsP'}
+                description={
+                  'MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS'
+                }
+              />
+            </div>
+          </div>
+          <div className={style.frontend}>
+            <div className={style.direction}>
+              <span className={style.direction__name}>{'Backend;'}</span>
+            </div>
+            <div className={style.cards}>
+              <TeamCard
+                image={slava}
+                name={'Slava Kagarlitskiy'}
+                telegram={'https://t.me/StelsP'}
+                github={'https://github.com/StelsP'}
+                description={
+                  'MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS'
+                }
+              />
+            </div>
+          </div>
+          <div className={style.frontend}>
+            <div className={style.direction}>
+              <span className={style.direction__name}>{'Design'}</span>
+            </div>
+            <div className={style.cards}>
+              <TeamCard
+                image={aleksey}
+                name={'Aleksey Popov'}
+                telegram={'https://t.me/AlekseyPopovDev'}
+                github={'https://github.com/1PopovAleksey'}
+                description={
+                  'MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS MADE THIS'
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Team;
