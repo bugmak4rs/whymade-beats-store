6b1f1ebf9dc4f80a1e8d712d784a3be9269ff25e
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-07 16:47:39 +0700

    feat: add Loops for Home

---
+++ src/components/Home/Loops/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import ProdCard from '../../Cards/Home/ProdCard';
+
+import iayze from '../../../assets/img/beats/iayze.png';
+import slumps from '../../../assets/img/beats/6slumps.png';
+import kencarson from '../../../assets/img/beats/ken-carson.png';
+import richamiri from '../../../assets/img/beats/rich-amiri.png';
+
+import style from './Loops.module.scss';
+
+const Loops: FC = () => {
+  return (
+    <section className={style.prod} id={'prod'}>
+      <div className={style.container}>
+        <div className={style.offset}>
+          <h2 className={style.title}>Loops</h2>
+          <div className={style.cards}>
+            <ProdCard image={iayze} name={'FrankSouth'} track={'UP!'} />
+            <ProdCard image={slumps} name={'Lil Down'} track={'hood&friends'} />
+            <ProdCard image={kencarson} name={'Young Nigga'} track={'planets'} />
+            <ProdCard image={richamiri} name={'Dudkid'} track={'Love off'} />
+          </div>
+          <div className={style.more}>
+            <Link className={style.more__link} to={'/prod'}>
+              <span>Show more</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Loops;
