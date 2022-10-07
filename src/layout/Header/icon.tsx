81540578bd44e7459f16dd4ea03473cd61cc30a3
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-07 16:48:00 +0700

    feat: add burger

---
+++ src/layout/Header/icon.tsx
@@ -0,0 +1,8 @@
+export const Burger = () => {
+  return (
+    <svg width="30" height="12" viewBox="0 0 30 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line y1="1" x2="30" y2="1" stroke="white" stroke-width="2" />
+      <line y1="11" x2="30" y2="11" stroke="white" stroke-width="2" />
+    </svg>
+  );
+};
