ec4dbbae5e130536ab15d9cbb56a47d2bdf1877a
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-07 18:37:24 +0700

    feat: add prod

--- src/layout/Main/index.tsx
+++ src/layout/Main/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from '../../pages/Home';
 import Beats from '../../pages/Beats';
+import Prod from '../../pages/Prod';
 import LoopKits from '../../pages/LoopKits';
 import AboutMe from '../../pages/AboutMe';
 import Team from '../../pages/Team';
@@ -14,6 +15,7 @@ const Main: FC = () => {
       <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/beats'} element={<Beats />} />
+        <Route path={'/prod'} element={<Prod />} />
         <Route path={'/loops'} element={<LoopKits />} />
         <Route path={'/about'} element={<AboutMe />} />
         <Route path={'/team'} element={<Team />} />
