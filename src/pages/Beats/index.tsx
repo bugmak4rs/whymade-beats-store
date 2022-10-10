cd5a2e15b2a451e8b1845e8bb644a7f335a3c6fd
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-10 20:58:15 +0700

    feat: update page Beats

--- src/pages/Beats/index.tsx
+++ src/pages/Beats/index.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react';
 
+import style from './Beats.module.scss';
+
 const Beats: FC = () => {
-  return <></>;
+  return (
+    <section className={style.beats} id={'beats'}>
+      <div className={style.container}>
+        <div className={style.offset}></div>
+      </div>
+    </section>
+  );
 };
 
 export default Beats;
