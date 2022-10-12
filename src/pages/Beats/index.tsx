1336c821806b7eb2e8dc9cbbd50596d6b510348a
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-12 21:28:31 +0700

    feat: add templete

--- src/pages/Beats/index.tsx
+++ src/pages/Beats/index.tsx
@@ -6,7 +6,11 @@ const Beats: FC = () => {
   return (
     <section className={style.beats} id={'beats'}>
       <div className={style.container}>
-        <div className={style.offset}></div>
+        <div className={style.offset}>
+          <div>Сюда ФИЛЬТР</div>
+          <div>Сюда ВСЕ БИТЫ</div>
+          Всё для тебя:)))
+        </div>
       </div>
     </section>
   );
